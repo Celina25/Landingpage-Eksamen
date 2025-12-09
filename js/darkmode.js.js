50c3988dc1f109004a4740d5e619190d1eaf0089
Celina25 <eaa25celu@students.eaaa.dk>
2025-12-09 20:32:47 +0100

    Ændring af farvevælger dark mode

--- js/darkmode.js.js
+++ js/darkmode.js.js
@@ -23,3 +23,62 @@ function updateCarousel() {
   const moveAmount = index * itemWidth;
   track.style.transform = `translateX(-${moveAmount}px)`;
 }
+
+
+
+// Favevalg til højtaleren //
+
+const colors = [
+  {
+    title: "Moonlight White",
+    color: "white",
+    hex: "#EBDEC6",
+    image: "Højtaler som gave moonliight white.png",
+
+  },
+{
+  title: "Chocolate Brown", 
+color: "brown",
+hex: "#282119",
+image: "Højtaler som gave chocolate brown.png",
+
+},
+{
+    title: "Dusty Rose",
+    color: "pink",
+    hex: "#DDA1A2",
+    image: "Højtaler som gave dusty rose.png",
+
+  },{
+    title: "Lavender Mist",
+    color: "purple",
+    hex: "#C3BFD5",
+    image: "Højtaler som gave lavender mist.png",
+
+  },{
+    title: "Sage Green",
+    color: "grøn",
+    hex: "#B2AC88",
+    image: "Højtaler som gave sage green.png",
+
+  },
+];
+
+const colorPicker = document.querySelector("#color-picker");
+const selectedColorTitle = document.querySelector("#selected-color-title");
+const speakerImage = document.querySelector("#selected-speaker-image");
+
+
+// Gennemløb farverne og opret knapper for hver farve//
+for (const color of colors) {
+    const html = `<button class="color-option" style="background-color: ${color.hex};"></button>`;
+    colorPicker.insertAdjacentHTML("beforeend", html);
+
+    const colorOption = colorPicker.lastElementChild;
+
+    colorOption.onclick = () => {
+        selectedColorTitle.textContent = color.title;
+        speakerImage.src = color.image;
+    };
+  }
+  
