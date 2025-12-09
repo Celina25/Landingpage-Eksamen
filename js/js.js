1d21715208e07dab451832587227623cec49ac04
Celina25 <eaa25celu@students.eaaa.dk>
2025-12-09 16:24:10 +0100

    Ændringer med buttons

--- js/js.js
+++ js/js.js
@@ -1,3 +1,7 @@
+
+
+// Billede karrusel //
+
 const track = document.querySelector(".carousel-track");
 const images = Array.from(track.children);
 
@@ -23,3 +27,32 @@ function updateCarousel() {
   const moveAmount = index * itemWidth;
   track.style.transform = `translateX(-${moveAmount}px)`;
 }
+
+// Favevalg til højtaleren //
+
+const colors = [
+  {
+    title: "Moonlight White",
+    color: "white",
+    hex: "#EBDEC6",
+    image: "img/Billeder af højtaler/White Lumina png.png"
+
+  },
+{
+  title: "Chocolate Brown", 
+color: "brown",
+hex: "#282119",
+image: "/Billeder af højtaler/Brown Lumina png.png",
+},
+];
+
+const colorPicker = document.querySelector("#color-picker");
+const selectedColorTitle = document.querySelector("#selected-color-title");
+const speakerImage = document.querySelector("#selected -speaker-image");
+
+// Gennemløb farverne og opret knapper for hver farve//
+for (const color of colors) {
+  const html = /*html*/ `<button class="color-option" style="background-color: ${color.hex};"></button>`;
+  colorPicker,insertAdjacentHTML("beforeend", html); // Tilføj knap til farvevalg//
+}
+
